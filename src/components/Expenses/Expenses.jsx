b18eca7e9e7cfc51e990ecad88916fd2425ad260
Pedro Batista <pedrobatista.420@gmail.com>
2021-04-20 14:46:47 +0100

    added a component to filter the year of the expenses

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem/ExpenseItem';
+import ExpensesFilter from './ExpenseFilter/ExpenseFilter';
 import Card from '../UI/Card/Card';
 
 import './Expenses.css';
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   const expenseItemList = props.expenses.map((expense) => {
     return (
       <ExpenseItem
@@ -15,7 +24,17 @@ function Expenses(props) {
     );
   });
 
-  return <Card className="expenses">{expenseItemList}</Card>;
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expenseItemList}
+      </Card>
+    </div>
+  );
 }
 
 export default Expenses;
--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -28,7 +28,7 @@ const ExpenseForm = (props) => {
       date: new Date(enteredDate),
     };
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
